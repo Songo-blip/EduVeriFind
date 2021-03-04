aae1fd592dd86aef7346ac66d5633fba06baf87a
lucas <lucas@DESKTOP-RJOEATO>
2021-03-04 20:33:39 +0200

    Add project files.

---
+++ AuthSystem/Controllers/UploadController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthSystem.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public class FileUploadController : Controller
+        {
+
+    [HttpPost("FileUpload")]
+            public async Task<IActionResult> Index(List<IFormFile> files)
+            {
+                long size = files.Sum(f => f.Length);
+
+                var filePaths = new List<string>();
+                foreach (var formFile in files)
+                {
+                    if (formFile.Length > 0)
+                    {
+ 
+                        var filePath = Path.GetTempFileName(); 
+                        filePaths.Add(filePath);
+
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await formFile.CopyToAsync(stream);
+                        }
+                    }
+                }
+
+                return Ok(new { count = files.Count, size, filePaths });
+            }
+        }
+    }
+}
+
+
